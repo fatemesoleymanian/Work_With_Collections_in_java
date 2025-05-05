d3d3922f266c6f5030056db63e2e3caae808b199
fatemesoleymanian <soleymanian.usc@gmail.com>
2025-05-05 13:08:38 +0330

    Inventory management is done

--- InventoryManagementSystem/src/main/java/org/example/Main.java
+++ InventoryManagementSystem/src/main/java/org/example/Main.java
@@ -4,41 +4,79 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Product> products = loadProducts();
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("enter product fields:");
-//        System.out.print("ID: ");
-//        String id = scanner.nextLine();
-//        System.out.print("Name: ");
-//        String name = scanner.nextLine();
-//        System.out.print("Quantity: ");
-//        int quantity = scanner.nextInt();
-//        System.out.print("Price: ");
-//        double cost = scanner.nextInt();
-//        scanner.nextLine();
-//        Product product = new Product(id,name,quantity,cost);
-//        add(products,product);
-//        System.out.println("hi");
-        System.out.println(products);
-        System.out.println("Looking for a product? give me the id");
-        String idd = scanner.nextLine();
-        Product found = search(products,idd);
-        if (found != null){
-            System.out.println(found);
-        }else System.out.println("Nothing match found!");
+        System.out.println("What do you want to do in this inventory?");
+        String choose;
+        do {
+            System.out.println("a) Add product\nb) Find product\nc) Show all products\nd) Update product");
+            System.out.println("e) Delete product\nf) Sort\ng) " +
+                    "Filter by quantity\nh) Grouping by category\ni) Exit");
+            System.out.print("Enter your choice: ");
+            choose = scanner.nextLine().toLowerCase();
 
+            switch (choose){
+                case "a":
+                     add(products,scanner);
+                     break;
+                case "b":
+                    Product found = search(products,scanner);
+                    if (found != null){
+                        System.out.println(found);
+                    }else System.out.println("Nothing match found!");
+                    break;
+                case "c":
+                    showAll(products);
+                    break;
+                case "d":
+                    update(products,scanner);
+                    break;
+                case "e":
+                    delete(products,scanner);
+                    break;
+                case "f":
+                    sortByPrice(products);
+                    break;
+                case "g":
+                    filterByLowStock(products,scanner);
+                    break;
+                case "h":
+                    groupByCategory(products);
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+                    break;
+            }
+
+        }while (!choose.equals("i"));
+        showAll(products);
         backup(products);
     }
-    public static void add(List<Product> products,Product product){
+    public static Product askForProduct(Scanner scanner){
+        System.out.println("enter product fields:");
+        System.out.print("ID: ");
+        String id = scanner.nextLine();
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Quantity: ");
+        int quantity = scanner.nextInt();
+        System.out.print("Price: ");
+        double cost = scanner.nextInt();
+        scanner.nextLine();
 
-            for (Product productt: products) {
+        String category = scanner.nextLine();
+        Product product = new Product(id,name,quantity,cost,category);
+        return product;
+    }
+    public static void add(List<Product> products,Scanner scanner){
+        Product product = askForProduct(scanner);
+
+        for (Product productt: products) {
                 if(productt.getId().equalsIgnoreCase(product.getId())){
                     System.out.println("product exists");
                     return;
@@ -47,13 +85,22 @@ public class Main {
             products.add(product);
             System.out.println("product added");
     }
-    public static Product search(List<Product> products,String id){
+    public static Product search(List<Product> products,Scanner scanner,String id ){
+        String idd = id;
+        if (id == null) {
+            System.out.println(products);
+            System.out.println("Looking for a product? give me the id");
+            idd = scanner.nextLine();
+        }
         for (Product productt: products) {
-            if (productt.getId().equalsIgnoreCase(id)){
+            if (productt.getId().equalsIgnoreCase(idd)){
                 return productt;
             }
         }
         return null;
+    }
+    public static Product search(List<Product> products, Scanner scanner) {
+        return search(products, scanner, null); // Call the original method with 'id' as null
     }
     public static List<Product> loadProducts(){
         Path productsFile = Paths.get("products.txt");
@@ -82,6 +129,83 @@ public class Main {
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void showAll(List<Product> products){
+        if (products.isEmpty()){
+            System.out.println("Inventory is empty!");
+        }else {
+            for (Product product : products) {
+                System.out.println(product);
+            }
+        }
+    }
+    public static void update(List<Product> products,Scanner scanner){
+        Product product = askForProduct(scanner);
+        Product searchedProduct = search(products,scanner,product.getId());
+        if (searchedProduct != null){
+            searchedProduct.setName(product.getName());
+            searchedProduct.setQty(product.getQty());
+            searchedProduct.setCost(product.getCost());
+            System.out.println(searchedProduct);
+        }else System.out.println("No such product");
+    }
+    public static void delete(List<Product> products,Scanner scanner) {
+        System.out.print("ID for delete: ");
+        String id = scanner.nextLine();
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()){
+            Product p = iterator.next();
+            if (p.getId().equalsIgnoreCase(id)){
+                iterator.remove();
+                System.out.println("product was deleted");
+                return;
+            }
+        }
+        System.out.println("Product not found");
+    }
+
+    public static void sortByPrice(List<Product> products){
+        if (products.isEmpty()) {
+            System.out.println("Products is empty");
+            return;
+        }
+        List<Product> sortedList = new ArrayList<>(products);
+        sortedList.sort(Comparator.comparing(Product::getCost).reversed());
+        System.out.println("sorted products by price");
+        System.out.println(sortedList);
+    }
+    public static void filterByLowStock(List<Product> products,Scanner scanner){
+        System.out.println("enter the threshold:");
+        int threshold = scanner.nextInt();
+        scanner.nextLine();
+        if (products.isEmpty()){
+            System.out.println("Products is empty");
+            return;
+        }
+        boolean found = false;
+        System.out.println("Products with quantity less than :"+threshold);
+        for (Product product: products){
+            if (product.getQty() < threshold){
+                System.out.println(product);
+                found = true;
+            }
+        }
+        if (!found) System.out.println("No product found with low stock");
+
+    }
+
+    public static void groupByCategory(List<Product> products) {
+        Map<String,List<Product>> grouped = new TreeMap<>();
+        for (Product product : products){
+            grouped.computeIfAbsent(product.getCategory(), p -> new ArrayList<>()).add(product);
+        }
+        for (Map.Entry<String,List<Product>> entry : grouped.entrySet()){
+            System.out.println("Category: "+ entry.getKey());
+            for (Product p : entry.getValue()){
+                System.out.println(p);
+            }
         }
     }
 }
--- InventoryManagementSystem/src/main/java/org/example/Product.java
+++ InventoryManagementSystem/src/main/java/org/example/Product.java
@@ -7,16 +7,16 @@ public class Product implements Serializable {
     private String name;
     private int qty;
     private double cost;
+    private String category;
 
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", qty=" + qty +
-                ", cost=" + cost +
-                '}';
+    public String getCategory() {
+        return category;
     }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 
     public String getId() {
         return id;
@@ -50,13 +50,26 @@ public class Product implements Serializable {
         this.cost = cost;
     }
 
-    public Product(String id, String name, int qty, double cost) {
+    public Product(){
+
+    }
+
+    public Product(String id, String name, int qty, double cost, String category) {
         this.id = id;
         this.name = name;
         this.qty = qty;
         this.cost = cost;
+        this.category = category;
     }
-    public Product(){
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", qty=" + qty +
+                ", cost=" + cost +
+                ", category='" + category + '\'' +
+                '}';
     }
 }
